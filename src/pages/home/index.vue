129b77b2aaa9f5c8d81627bd984f4fdacfa46640
wangshuaixue <wsx_xue@163.com>
2023-08-16 15:30:23 +0800

    首页添加

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -2,14 +2,33 @@
  * @author: wangshuaixue
  * @Date: 2023-07-04 14:37:53
  * @description: 
- * @LastEditTime: 2023-08-14 14:55:52
+ * @LastEditTime: 2023-08-16 15:29:20
  * @LastEditors: wangshuaixue
  * @FilePath: /draw/src/pages/home/index.vue
 -->
 <template>
   <view class="content">
-    <view class="text-area">
-      <text class="title">首页</text>
+    <view class="box">
+      <image
+        class="img"
+        src="https://file.tangchenglive.com/uploadfile/20230707/535841053627027457.png"
+      ></image>
+      <view class="desc">标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题</view>
+      <view class="line-wrap">
+        <view class="user-wrap">
+          <image
+            class="avat"
+            src="https://file.tangchenglive.com/uploadfile/20230707/535841053627027457.png"
+          ></image>
+          <text>昵称 名称1111</text>
+        </view>
+        <uni-icons
+          type="heart"
+          size="20"
+          color="#FF7C33"
+        ></uni-icons>
+        <!-- <uni-icons type="heart-filled" size="20" color="#FF7C33"></uni-icons> -->
+      </view>
     </view>
   </view>
 </template>
@@ -26,30 +45,46 @@
   };
 </script>
 
-<style>
+<style lang="scss" scoped>
 .content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  padding: 0 20rpx;
 }
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
+.box {
+  background: #fff;
+  border-radius: 10rpx;
+  overflow: hidden;
+  box-shadow: 0rpx 10rpx 24rpx 0rpx rgba(225, 225, 225, 0.5);
+  .desc {
+    padding: 0 10rpx;
+    margin-top: 10rpx;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .line-wrap {
+    padding: 0 10rpx 14rpx;
+    margin-top: 10rpx;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .user-wrap {
+    display: flex;
+    align-items: center;
+    font-size: 26rpx;
+    .avat {
+      width: 60rpx;
+      height: 60rpx;
+      border-radius: 50%;
+      overflow: hidden;
+      margin-right: 10rpx;
+    }
+  }
 }
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+.img {
+  width: 100%;
+  height: 400rpx;
+  object-fit: cover;
+  border-radius: 14rpx;
 }
 </style>
